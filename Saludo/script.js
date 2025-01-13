d8a44bd87bbe07b0de619d4b0e56523cb1b3d021
hugomonntes <huugomontees@gmail.com>
2025-01-13 09:49:05 +0100

    Update

--- Saludo/script.js
+++ Saludo/script.js
@@ -3,3 +3,11 @@ function mandarSaludo(){
     window.alert("Hola " + nombre);
     document.write(`Hola ${nombre}`);
 }
+
+
+let boton = document.getElementById("boton");
+boton.addEventListenner("onclick", cambiarColor());
+function cambiarColor(){
+    let body = document.getElementById("body");
+    body.style.backgroundColor = "red";
+}
