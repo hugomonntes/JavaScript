55e91dc28b95373839126e5cb6e61d02090154fe
hugomonntes <huugomontees@gmail.com>
2025-01-13 10:39:11 +0100

    Update

--- Saludo/script.js
+++ Saludo/script.js
@@ -11,3 +11,10 @@ function cambiarColor(){
     let body = document.getElementById("body");
     body.style.backgroundColor = "red";
 }
+
+let dni = "12345678Z"; //For que recorra el String hasta la posicion 7 que son los numero y estos los almacene en un varible numeros y que en la posiucion 8 que es la ultima la almacene en la varable de letras.
+let numeros = [];
+for (let i = 0; i < dni[7]; i++) {
+    numeros = dni[i];
+}
+console.log(numeros);
