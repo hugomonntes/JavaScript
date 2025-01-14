3f338c774aabb913ceda4d7e781f9d5e1de04db4
hugomonntes <huugomontees@gmail.com>
2025-01-14 10:46:05 +0100

    Update

--- Saludo/script.js
+++ Saludo/script.js
@@ -1,8 +1,8 @@
-function mandarSaludo(){
-    let nombre = prompt("Introduce tu nombre");
-    window.alert("Hola " + nombre);
-    document.write(`Hola ${nombre}`);
-}
+// function mandarSaludo(){
+//     let nombre = prompt("Introduce tu nombre");
+//     window.alert("Hola " + nombre);
+//     document.write(`Hola ${nombre}`);
+// }
 
 
 let boton = document.getElementById("boton");
@@ -12,7 +12,9 @@ function cambiarColor(){
     body.style.backgroundColor = "red";
 }
 
-let dni = "12345678Z"; //For que recorra el String hasta la posicion 7 que son los numero y estos los almacene en un varible numeros y que en la posiucion 8 que es la ultima la almacene en la varable de letras.
+let dni = "12345678Z";
+let combinacionLetras = ["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"];
+let letra = dni[dni.length];
 let numeros = [];
 for (let i = 0; i < dni[7]; i++) {
     numeros.push(dni[i]);
