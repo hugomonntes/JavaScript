810449d3f878143990af0dc321959f0966f1055d
hugomonntes <huugomontees@gmail.com>
2025-01-20 10:46:59 +0100

    Update

---
+++ Exercises_Bol1/FuncionesBol1.js
@@ -0,0 +1,5 @@
+//Ejercicio 1
+function mostrarNumero(){
+    window.alert(Math.ceil(Math.random()*100));
+}
+
---
+++ JS_Ex2/arrays.js
@@ -0,0 +1,8 @@
+function bucle(){
+    let matriz = [[1, 2, 3, 4, 5 ,6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18]];
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            console.log(matriz[i][j]);            
+        }
+    }
+}
