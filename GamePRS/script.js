90cacb8a985b68769f552969d1b11869695bc413
hugomonntes <huugomontees@gmail.com>
2025-02-19 12:22:25 +0100

    Update

--- GamePRS/script.js
+++ GamePRS/script.js
@@ -1,1 +1,22 @@
 //Piedra papel tijeras
+
+function turnoUsuario(){
+    let isChecked = true;
+    let options = ["rock", "paper", "scissors"]
+    let prompt; //Almacenar datos usuario
+    do{
+        if(prompt == options[0]){
+            window.alert("Rock");
+            isChecked = true;
+        } else if (prompt == options[1]){
+            window.alert("Paper");
+            isChecked = true;
+        } else if (prompt == options[2]){
+            window.alert("Scissors");
+            isChecked = true;
+        } else {
+            window.alert("false");
+            isChecked = false;
+        }
+    } while(!isChecked);
+}
