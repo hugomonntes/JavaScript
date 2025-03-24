9a01f2a9c210c3cf5e32e3eaa998540a20ad3709
hugomonntes <huugomontees@gmail.com>
2025-03-24 10:40:01 +0100

    Update

--- EjercicioFiguras/script.js
+++ EjercicioFiguras/script.js
@@ -1,4 +1,51 @@
-function cambiarURL(){
-    let objeto3D = document.getElementById("3d");
-    objeto3D.style.display = "block";
-}
+let container = document.getElementsByClassName("div1")[0];
+let boton = document.getElementsByName("enviar")[0];
+let divResultados = document.getElementById("resultados");
+let desplegable = document.getElementsByName("desplegable")[0];
+divResultados.style.color = "white";
+desplegable.addEventListener("click", () => {
+    let opcionDesplegable = desplegable.options[desplegable.selectedIndex].value;
+    console.log(opcionDesplegable);
+    if(opcionDesplegable == "Cylindre"){
+        divResultados.innerHTML = `
+        <p>Radio: </p>
+        <input type="text" id="radio" name="radio" placeholder="1">
+        <p>Altura: </p>
+        <input type="text" id="altura" name="altura" placeholder="1">
+        <p>Resultado: </p>
+        <input readonly type="text" id="resultado" name="resultado" >
+        `;
+    } else if(opcionDesplegable == "Cube"){
+        divResultados.innerHTML = `
+        <p>Lado: </p>
+        <input type="text" id="lado" name="lado" placeholder="1">
+        <p>Resultado: </p>
+        <input readonly type="text" id="resultado" name="resultado" >
+        `;
+    } else if(opcionDesplegable == "Cone"){
+        divResultados.innerHTML = `
+        <p>Radio: </p>
+        <input type="text" id="radio" name="radio" placeholder="1">
+        <p>Altura: </p>
+        <input type="text" id="altura" name="altura" placeholder="1">
+        <p>Resultado: </p>
+        <input readonly type="text" id="resultado" name="resultado" >
+        `;
+    } else if (opcionDesplegable == "Sphere"){
+        divResultados.innerHTML = `
+        <p>Diametro: </p>
+        <input type="text" id="diametro" name="diametro" placeholder="1">
+        <p>Resultado: </p>
+        <input readonly type="text" id="resultado" name="resultado" >
+        `;
+    } else if (opcionDesplegable == "Rectangular"){
+        divResultados.innerHTML = `
+        <p>Radio: </p>
+        <input type="text" id="radio" name="radio" placeholder="1">
+        <p>Altura: </p>
+        <input type="text" id="altura" name="altura" placeholder="1">
+        <p>Resultado: </p>
+        <input readonly type="text" id="resultado" name="resultado" >
+        `;
+    }
+});
